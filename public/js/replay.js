1d7c235b9d6362f20f09ff063aeb6a4e7ff0b662
TheUberKid <zekixu@gmail.com>
2017-08-09 00:04:12 -0700

    additional web design on replay viewer

--- public/js/replay.js
+++ public/js/replay.js
@@ -1,3 +1,5 @@
+var mapBanner = document.getElementById('map-banner');
+var map = document.getElementById('map');
 var gametype = document.getElementById('gametype');
 var date = document.getElementById('date');
 var build = document.getElementById('build');
@@ -7,9 +9,13 @@ var team0level = document.getElementById('team0level');
 var team1level = document.getElementById('team1level');
 var team0bar = document.getElementById('level-blue');
 var team1bar = document.getElementById('level-red');
+var team0players = document.getElementById('team0players')
+var team1players = document.getElementById('team1players')
 
 var timePlayed = new Date(params.TimePlayed);
 
+mapBanner.style.backgroundImage = ('url("/img/mapbanners/' + params.MapName + '.jpg")');
+map.innerHTML = params.MapName;
 gametype.innerHTML = fullGametypes[params.GameType];
 date.innerHTML = timePlayed.getUTCDate() + ' ' + months[timePlayed.getMonth()] + ' ' + timePlayed.getFullYear();
 build.innerHTML = params.Build;
@@ -19,3 +25,24 @@ team0level.innerHTML = params.Team0Level;
 team1level.innerHTML = params.Team1Level;
 team0bar.style.flexGrow = params.Team0Level - 5;
 team1bar.style.flexGrow = params.Team1Level - 5;
+
+// players
+var resArr = ['', ''];
+for(var i in params.Players){
+  var res = '';
+  var p = params.Players[i];
+
+  var Hero = p.Hero;
+  if(altNames[Hero]) Hero = altNames[Hero];
+  var BattleTag = p.BattleTag.split('#')[0];
+
+  res += '<div class="row team' + p.Team + ' player" data-battletag="' + p.BattleTag + '">';
+  res += '<div class="item replayHero">' + Hero + '</div>';
+  res += '<div class="item replayBattleTag">' + p.BattleTag.split('#')[0] + '</div>';
+  if(p.MVP) res += '<div class="item replayMVP"> MVP </div>'
+  res += '</div>';
+
+  resArr[p.Team === 0 ? 0 : 1] += res;
+}
+team0players.innerHTML = resArr[0];
+team1players.innerHTML = resArr[1];
